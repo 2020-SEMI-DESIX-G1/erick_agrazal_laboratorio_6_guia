86ff80e9a3c24adb83967b61eb09c7e40020dcde
Erick Agrazal <erick.agrazal@gmail.com>
2020-06-11 19:42:24 -0500

    Added initial routes.

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const ESTUDIANTES = [
+    {
+        nombre: "Erick Agrazal",
+        edad: 29,
+    },
+    {
+        nombre: "Luis García",
+        edad: 22,
+    }
+];
+
+// Intermediario
+app.use(bodyParser.json());
+
+
+// Controladores
+app.get('/api/estudiantes/', (req, res) => {
+    res.json({
+        cantidad: ESTUDIANTES.length,
+        estudiantes: ESTUDIANTES
+    });
+});
+app.get('/api/estudiantes/:indice', (req, res) => {
+    res.json(ESTUDIANTES[req.params.indice]);
+});
+app.post('/api/estudiantes/', (req, res) => {
+    ESTUDIANTES.push(req.body);
+    res.json(req.body);
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
